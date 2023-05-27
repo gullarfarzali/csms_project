15a0da822902ef888fc19b91adafe4858e193cdf
gullarfarzali <134553528+gullarfarzali@users.noreply.github.com>
2023-05-27 22:16:14 +0400

    add_new_service.php

--- add_new_service.php
+++ add_new_service.php
@@ -4,7 +4,7 @@ session_start();
 
 require_once "admin_auth_check.php";
 
-require __DIR__ . "\\..\\database.php";
+require __DIR__ . "database.php";
 
 if (isset($_POST["submit"]) && isset($_POST["total"]) && isset($_POST["service_name"])) {
     $service_name = $_POST['service_name'];
