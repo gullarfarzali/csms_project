496e0a86efb286789d99b5d2e86bb5574a87863c
gullarfarzali <134553528+gullarfarzali@users.noreply.github.com>
2023-05-27 22:31:17 +0400

    Update manage_statuses.php

--- manage_statuses.php
+++ manage_statuses.php
@@ -97,7 +97,7 @@
                     <tbody>
                     <?php
 
-                    require __DIR__ . '\\..\\database.php';
+                    require __DIR__ . 'database.php';
 
                     session_start();
 
